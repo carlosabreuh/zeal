a6a7feb4e6d65c08af21e510a0ca55c2c9a1515d
carlosabreuh <carlos@abreuh.com>
2021-02-23 22:16:58 -0500

    Axios

--- src/App.js
+++ src/App.js
@@ -105,8 +105,9 @@ function Footer() {
       <Typography className={classes.footerText} color="textSecondary">
    
 
-<p>All information in the Service is provided &quot;as is&quot;, with no guarantee of completeness, accuracy, timeliness or of the results obtained from the use of this information, and without warranty of any kind, express or implied, including, but not limited to warranties of performance, merchantability and fitness for a particular purpose.</p>
-<p>The Company will not be liable to You or anyone else for any decision made or action taken in reliance on the information given by the Service or for any consequential, special or similar damages, even if advised of the possibility of such damages.</p>
+All information in the Service is provided &quot;as is&quot;, with no guarantee of completeness, accuracy, timeliness or of the results obtained from the use of this information, and without warranty of any kind, express or implied, including, but not limited to warranties of performance, merchantability and fitness for a particular purpose.
+<br></br>
+The Company will not be liable to You or anyone else for any decision made or action taken in reliance on the information given by the Service or for any consequential, special or similar damages, even if advised of the possibility of such damages.
 
       </Typography>
     </footer>
