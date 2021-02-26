40843f74eb5f1f31da7e3348d5c32ce4a5d40067
carlosabreuh <carlos@abreuh.com>
2021-02-26 13:21:37 -0500

    Create Home.jsx

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Intro,  } from "../../components/index";
+
+
+export default function Home() {
+  return (
+    <div>
+      <Intro /> 
+      
+      
+      
+    </div>
+    
+  );
+}
