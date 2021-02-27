1772c4acd4a3a6b1b6e1079e33dd881613f3c9c4
carlosabreuh <carlos@abreuh.com>
2021-02-27 13:02:02 -0500

    Update Intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,12 +5,18 @@ export default function Intro() {
   return (
     <Box p={2}>
       <Typography>
-        Zeal provides real-time results by directing you straight to the source of the services you need.  This will cut down the time in which you'll spend searching the internet for different types of assistance. 
+        Zeal is where you can find local charities and food pantries accepting donations as well as donating to those in need.  There are two ways you can use this tool:
       </Typography>
       <ol>
         <li>
           <Typography>
-            Click the Resources link to be immediately redirected to the list of resource assistance sites. 
+            Click the Resources link to be immediately redirected to the finder.  There you will enter your Zipcode and find your local resources.  
+          </Typography>
+        </li>
+        <br></br>
+        <li>
+          <Typography>
+            Click on the resources below, which are static sites that can provide further assistance in finding other resources.  
           </Typography>
         </li>
       </ol>
