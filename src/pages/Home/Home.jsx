9a3de51030db173bcfeca07b3bcfa327d686b159
carlosabreuh <carlos@abreuh.com>
2021-03-01 01:15:15 -0500

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 
-import { Intro,  } from "../../components/index";
-import Statics from '../../components/Statics'
-import Card from '../../components/Card'
-import Nav from '../../Navbar/Nav'
-
+import { Intro } from '../../components/index';
+import Statics from '../../components/Statics';
+import Card from '../../components/Card';
+import Nav from '../../Navbar/Nav';
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
 
 export default function Home() {
   return (
     <div>
-      
-      <Intro /> 
+      <Intro />
       <Statics />
       {/* <Card /> */}
-      
-      
+
+      <Typography>
+        <ul>
+          <li>
+            Maintained by Carlos Abreu, who can be reached at{' '}
+            <Link href='mailto:carlos@abreuh.com'>carlos@abreuh.com</Link>
+          </li>
+        </ul>
+      </Typography>
     </div>
-    
   );
 }
