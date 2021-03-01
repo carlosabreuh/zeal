d7073f48cf0956d32169ef0c9b2a546b64d6a202
carlosabreuh <carlos@abreuh.com>
2021-03-01 00:45:15 -0500

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
 import { Intro,  } from "../../components/index";
+import Statics from '../../components/Statics'
+import Card from '../../components/Card'
+import Nav from '../../Navbar/Nav'
 
 
 export default function Home() {
   return (
     <div>
-      <Intro /> 
       
+      <Intro /> 
+      <Statics />
+      {/* <Card /> */}
       
       
     </div>
