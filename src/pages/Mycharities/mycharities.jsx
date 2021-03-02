62ca7ef0de6e10f03ab252502a33fe21965c109b
carlosabreuh <carlos@abreuh.com>
2021-03-02 14:29:45 -0500

    Update mycharities.jsx

--- src/pages/Mycharities/mycharities.jsx
+++ src/pages/Mycharities/mycharities.jsx
@@ -1,17 +1,29 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { StatusTable } from '../../components';
 import Alert from '../../components/Alert'
 
 export default function MyCharities({ myCharities, tableRows, setMyCharities }) {
   console.log(myCharities);
-
+useEffect(() => {
+axios 
+          .get(`http://localhost:8080/api/users/`)
+          .then((res) => {
+            console.log(res);
+      
+            setMyCharities(res.data);
+          })
+          .catch((error) => console.log(error));
+      
+    
+  },[])
   return (
       <>
     { myCharities.length ? <StatusTable
       setMyCharities={setMyCharities}
       tableRows={myCharities}
       page={"MyCharities"}
-    />: ""}
+    />: <div></div>}
     </>
   );
 }
