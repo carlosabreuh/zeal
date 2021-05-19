dc6a6ea4e8b8f1987daa1fe68c5ff6db7c6989c9
carlosabreuh <carlos@abreuh.com>
2021-05-19 17:44:09 -0400

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
-import Nav from '../../src/Navbar/Nav'
+
 
 
 
