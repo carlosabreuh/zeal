b0ae750e8ee6769d36b02cb4cd438ff19e788783
Carlos Abreu <carlos@abreuh.com>
2021-09-03 17:03:34 -0400

    unsused code

--- src/pages/Mycharities/mycharities.jsx
+++ src/pages/Mycharities/mycharities.jsx
@@ -1,29 +1,34 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { StatusTable } from '../../components';
-import Alert from '../../components/Alert'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { StatusTable } from "../../components";
 
-export default function MyCharities({ myCharities, tableRows, setMyCharities }) {
+export default function MyCharities({
+  myCharities,
+  tableRows,
+  setMyCharities,
+}) {
   console.log(myCharities);
-useEffect(() => {
-axios 
-          .get(`http://localhost:8080/api/users/`)
-          .then((res) => {
-            console.log(res);
-      
-            setMyCharities(res.data);
-          })
-          .catch((error) => console.log(error));
-      
-    
-  },[])
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/api/users/`)
+      .then((res) => {
+        console.log(res);
+
+        setMyCharities(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
   return (
-      <>
-    { myCharities.length ? <StatusTable
-      setMyCharities={setMyCharities}
-      tableRows={myCharities}
-      page={"MyCharities"}
-    />: <div></div>}
+    <>
+      {myCharities.length ? (
+        <StatusTable
+          setMyCharities={setMyCharities}
+          tableRows={myCharities}
+          page={"MyCharities"}
+        />
+      ) : (
+        <div></div>
+      )}
     </>
   );
 }
